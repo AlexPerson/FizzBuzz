7e56edde8a03d475befba716ea25ebd6a551a785
Alexander Person <alexanderperson@Alexanders-MacBook-Pro.local>
2016-01-12 17:27:36 +0000

    added view controller and game.play method now returns a tuple that is recognized by the view controller

---
+++ FizzBuzzTests/ViewControllerUnitTests.swift
@@ -0,0 +1,35 @@
+//
+//  ViewControllerUnitTests.swift
+//  FizzBuzz
+//
+//  Created by Alexander Person on 1/12/16.
+//  Copyright © 2016 Alexander Person. All rights reserved.
+//
+
+import XCTest
+
+class ViewControllerUnitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
